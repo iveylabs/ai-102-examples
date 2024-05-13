4606950e25da7dd43d925f646436a2241eea00f0
paul-iveylabs <paul@iveylabs.co.uk>
2024-05-13 17:20:34 +0100

    Fixed my search setup after azd config

--- 05-ai-search/001-MySetup/setup.cmd
+++ 05-ai-search/001-MySetup/setup.cmd
@@ -3,23 +3,23 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set values for your subscription and resource group
 set subscription_id=SUBSCRIPTION_ID
-set resource_group=SEARCH_RESOURCE_GROUP
+set resource_group=RESOURCE_GROUP
 set location=REGION
-set storage_account_name=STORAGE_ACCOUNT_NAME
-set multi_service_name=MULTI_ACCOUNT_NAME
-set search_service_name=SEARCH_KEY
+set storage_account_name=STORAGE_NAME
+set multi_service_name=MULTI_SERVICE_NAME
+set search_service_name=SEARCH_NAME
 
-echo Uploading files...
-rem Hack to get storage key
-for /f "tokens=*" %%a in ( 
-'az storage account keys list --subscription !subscription_id! --resource-group !resource_group! --account-name !storage_account_name! --query "[?keyName=='key1'].{keyName:keyName, permissions:permissions, value:value}"' 
-) do ( 
-set key_json=!key_json!%%a 
-) 
-set key_string=!key_json:[ { "keyName": "key1", "permissions": "Full", "value": "=!
-set AZURE_STORAGE_KEY=!key_string:" } ]=!
+@REM echo Uploading files...
+@REM rem Hack to get storage key
+@REM for /f "tokens=*" %%a in ( 
+@REM 'az storage account keys list --subscription !subscription_id! --resource-group !resource_group! --account-name !storage_account_name! --query "[?keyName=='key1'].{keyName:keyName, permissions:permissions, value:value}"' 
+@REM ) do ( 
+@REM set key_json=!key_json!%%a 
+@REM ) 
+@REM set key_string=!key_json:[ { "keyName": "key1", "permissions": "Full", "value": "=!
+@REM set AZURE_STORAGE_KEY=!key_string:" } ]=!
 
-call az storage blob upload-batch -d margies-travel -s data --account-name !storage_account_name! --auth-mode key --account-key %AZURE_STORAGE_KEY%  --output none
+@REM call az storage blob upload-batch -d margies-travel -s data --account-name !storage_account_name! --auth-mode key --account-key %AZURE_STORAGE_KEY%  --output none
 
 rem Set values for your Search service
 set url=https://!search_service_name!.search.windows.net
